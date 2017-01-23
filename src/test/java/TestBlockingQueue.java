7b50309b4ee037f7be34d6dc933596c7baa1a86a
vlnik-belyaev <vlnik.belyaev@gmail.com>
2017-01-23 16:04:21 +0200

    Test files were changed

---
+++ src/test/java/TestBlockingQueue.java
@@ -0,0 +1,49 @@
+import mriqueue.MostRecentlyInsertedBlockingQueue;
+import org.junit.Test;
+
+import java.util.Queue;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class TestBlockingQueue {
+
+    @Test
+    public void testEmptyQueue() {
+        Queue<Integer> queue = new MostRecentlyInsertedBlockingQueue<Integer>(3);
+        assertEquals(queue.size(),0);
+    }
+
+    @Test
+    public void testPeekEmptyQueue() {
+        Queue<Integer> queue = new MostRecentlyInsertedBlockingQueue<Integer>(3);
+        assertNull(queue.peek());
+    }
+
+    @Test
+    public void testPollEmptyQueue() {
+        Queue<Integer> queue = new MostRecentlyInsertedBlockingQueue<Integer>(3);
+        assertNull(queue.poll());
+    }
+
+    @Test
+    public void testPollQueue() {
+        Queue<Integer> queue = new MostRecentlyInsertedBlockingQueue<Integer>(3);
+        queue.offer(1);
+        assertEquals(queue.poll(),new Integer (1));
+        assertEquals(queue.size(),0);
+    }
+
+    @Test
+    public void testOfferToQueue() {
+        Queue<Integer> queue = new MostRecentlyInsertedBlockingQueue<Integer>(3);
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+        queue.offer(4);
+        queue.offer(5);
+        assertEquals(queue.size(),3);
+        assertArrayEquals(queue.toArray(), new Object[]{3, 4, 5});
+    }
+}
